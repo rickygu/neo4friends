f628aa95fa3f09442b4ee48f629f727af5f1c09a
rickyzgu <rickyzgu@gmail.com>
2015-12-15 19:55:28 -0800

    test user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User
   validates :number, numericality: { only_integer: true }
   
   has_many :both, :friends, model_class: :User, unique: true, type: :HAS_FRIENDS
-
+  
+  def make_friends(user)
+    # since Neo4j does not support undirected relationships
+    # We will sort user number to always make friends in the direction of smaller number user -> bigger number user
+    users = [self, user].sort! {|a, b| a.number <=> b.number }
+    users.first.friends << users.last
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,51 @@ RSpec.describe User, type: :model do
     user = User.create number: 1
     expect(user.number).to eq 1
   end
+  
+  it "cannot create user with same number" do
+    user = User.create number: 1
+    expect{User.create(number: 1)}.to raise_error(Neo4j::Server::CypherResponse::ConstraintViolationError)
+  end
+  
+  it "cannot create user with invalid id" do
+    expect{user = User.create! number: "abc"}.to raise_error(Neo4j::ActiveNode::Persistence::RecordInvalidError)
+  end
+  
+  it "can silently ignore creating user with invalid number" do
+    User.create number: "abc"
+    expect(User.count).to eq 0
+  end
+   
+  it "can add another user as friend" do
+    user = User.create number: 1
+    user2 = User.create number: 2
+    user.make_friends(user2)
+    expect(user.friends.first).to eq user2
+    expect(user2.friends.first).to eq user
+    expect(user.friends.count).to eq 1
+    expect(user2.friends.count).to eq 1
+  end
+  
+  it "cannot add another user as friend twice" do
+    user = User.create number: 1
+    user2 = User.create number: 2
+    user.make_friends user2
+    expect(user.friends.count).to eq 1
+    expect(user2.friends.count).to eq 1
+    user.make_friends user2
+    expect(user.friends.count).to eq 1
+    expect(user2.friends.count).to eq 1
+  end
+  
+  it "cannot add another user back as friend once friended" do
+    user = User.create number: 1
+    user2 = User.create number: 2
+    user.make_friends user2
+    expect(user.friends.count).to eq 1
+    expect(user2.friends.count).to eq 1
+    user2.make_friends user
+    expect(user.friends.count).to eq 1
+    expect(user2.friends.count).to eq 1
+  end
+  
 end
